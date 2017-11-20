2571ca266798c22a0308e6cf922b62cccc511409
FernandoSL <nando.leandro@hotmail.com>
2017-11-20 17:54:09 -0200

    Implementar serviço de criptografia.

---
+++ AllGoRithmFramework.Service/DomainServices/EncryptService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AllGoRithmFramework.Service.DomainServices
+{
+    public class EncryptService
+    {
+        private string Key { get; set; }
+
+        private string Iv { get; set; }
+
+        public EncryptService(string key, string iv)
+        {
+            this.Key = key;
+            this.Iv = iv;
+        }
+
+        public string Encrypt(string content)
+        {
+            string result;
+
+            using (var aes = Aes.Create())
+            {
+                aes.Key = ASCIIEncoding.ASCII.GetBytes(this. Key);
+                aes.IV = ASCIIEncoding.ASCII.GetBytes(this.Iv);
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                {
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(content);
+                            }
+
+                            var iv = aes.IV;
+
+                            var decryptedContent = msEncrypt.ToArray();
+
+                            byte[] byteResult = new byte[iv.Length + decryptedContent.Length];
+
+                            Buffer.BlockCopy(iv, 0, byteResult, 0, iv.Length);
+                            Buffer.BlockCopy(decryptedContent, 0, byteResult, iv.Length, decryptedContent.Length);
+
+                            result = Convert.ToBase64String(byteResult);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
